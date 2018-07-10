8a0681fd5292c9c90edf4e6dd0919b9dba163462
Jeremy Parker <jrobinparker@gmail.com>
2018-07-10 14:28:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_accessor :roster, :student, :grade
+  attr_reader :student, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = roster
+  end
+  
+  def roster
+    roster = {
+      :grade => [
+          :students => []
+          ]
+      }
+  end
+    
+  def add_student(student, grade)
+    roster[:grade][:students] << student
+  end 
+  
+end
